1652d45ea7fd2d7bd91c0a3cdfbe388bcad424de
Kemal Karagöz <52218326+kkrgzz@users.noreply.github.com>
2022-01-27 04:05:49 +0300

    Champion Selecting Done!

--- src/scrape/lol/main.py
+++ src/scrape/lol/main.py
@@ -3,20 +3,26 @@ import requests
 import json
 
 
+user_input = "JiNX"
+
+# Reading champion names and keys to fetch builds from web page.
+champions = open("champions.json")
+champions_data = json.load(champions)
+
+# Web Page URL section and downloads all data and parse them.
 main_url = "https://www.probuilds.net/champions/details/222"
-
 request = requests.get(main_url)
-
 soup = bs(request.text, "lxml")
+
+# Filtering classes which equals to bigData.
 items = soup.find_all(class_ = "bigData")
 
-all_items_arr = []
-
+# Stores item names and images of items, respectively
 item_names_arr = []
 item_images_arr = []
 
+# Parsing data
 for item_containers in items:
-
     item_names = item_containers.find_all(class_ = "item-name")
     for name in item_names:
         item_names_arr.append(name.text)
@@ -25,4 +31,7 @@ for item_containers in items:
     for img in item_imgs:
         item_images_arr.append(img.get("src"))
 
-    all_items_arr.append(item_containers)
+
+for champion in champions_data["champions"]:
+    if champion["name"].lower() == user_input.lower():
+        print(champion["name"], " - ", champion["key"])
